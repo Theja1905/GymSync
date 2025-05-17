6110402e27cf9ba3f9707f9e9ae1f62387404dbf
chyemavis <chyemavis@gmail.com>
2025-05-17 22:19:20 +0800

    Add firebase config

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCeF82pnOO0Xk90afb4fI6u8XPBXAv_E18",
+  authDomain: "gymsync-125a5.firebaseapp.com",
+  projectId: "gymsync-125a5",
+  storageBucket: "gymsync-125a5.firebasestorage.app",
+  messagingSenderId: "648350050567",
+  appId: "1:648350050567:web:36af206b7ba8949a20bda2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
