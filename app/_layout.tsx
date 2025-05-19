140c1eeb92edc832f4a1da11ff97c3d1dca3b7de
Theja1905 <153060692+Theja1905@users.noreply.github.com>
2025-05-19 21:31:38 +0800

    edits to tabs done!

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
-import { Tabs } from 'expo-router';
+import { Stack } from 'expo-router';
 
-export default function TabsLayout() {
+export default function RootLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }} />
-  );
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ title: "Log In" }} />
+      <Stack.Screen name="signup" options={{ title: "Sign Up" }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> 
+    </Stack>
+  );
 }
