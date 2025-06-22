495c8e3a3968448f6110918eb19e1e2869cf128f
chyemavis <chyemavis@gmail.com>
2025-06-22 17:29:14 +0800

    edits

--- app/(tabs)/logger/screens/workout.tsx
+++ app/(tabs)/logger/screens/workout.tsx
@@ -1,7 +1,15 @@
 import { useRouter } from 'expo-router';
 import { addDoc, collection } from 'firebase/firestore';
 import React, { useState } from 'react';
-import { Alert, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { db } from '../../../../firebase';
 
 export default function WorkoutScreen() {
@@ -65,7 +73,11 @@ export default function WorkoutScreen() {
         params: {
           routineTitle,
           exercises: JSON.stringify(
-            workoutData.exercises.map(({ name, sets, reps }) => ({ name, sets, reps }))
+            workoutData.exercises.map(({ name, sets, reps }) => ({
+              name,
+              sets,
+              reps,
+            }))
           ),
           workoutId: docRef.id,
         },
@@ -78,7 +90,10 @@ export default function WorkoutScreen() {
 
   return (
     <View style={styles.page}>
-      <ScrollView style={styles.scrollView} contentContainerStyle={styles.container}>
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.container}
+      >
         <Text style={styles.title}>Add Exercises</Text>
 
         <TextInput
@@ -90,9 +105,9 @@ export default function WorkoutScreen() {
 
         <View style={styles.card}>
           <View style={styles.headerRow}>
-            <Text style={styles.headerText}>Exercise</Text>
-            <Text style={styles.headerText}>Reps</Text>
-            <Text style={styles.headerText}>Sets</Text>
+            <Text style={styles.headerTextExercise}>Exercise</Text>
+            <Text style={styles.headerTextSmall}>Reps</Text>
+            <Text style={styles.headerTextSmall}>Sets</Text>
           </View>
 
           {exercises.map((exercise) => (
@@ -101,21 +116,27 @@ export default function WorkoutScreen() {
                 style={styles.inputExercise}
                 placeholder="Type"
                 value={exercise.name}
-                onChangeText={(text) => updateExercise(exercise.id, 'name', text)}
+                onChangeText={(text) =>
+                  updateExercise(exercise.id, 'name', text)
+                }
               />
               <TextInput
                 style={styles.inputSmall}
                 placeholder="Reps"
                 value={exercise.reps}
                 keyboardType="numeric"
-                onChangeText={(text) => updateExercise(exercise.id, 'reps', text)}
+                onChangeText={(text) =>
+                  updateExercise(exercise.id, 'reps', text)
+                }
               />
               <TextInput
                 style={styles.inputSmall}
                 placeholder="Sets"
                 value={exercise.sets}
                 keyboardType="numeric"
-                onChangeText={(text) => updateExercise(exercise.id, 'sets', text)}
+                onChangeText={(text) =>
+                  updateExercise(exercise.id, 'sets', text)
+                }
               />
               <TouchableOpacity
                 onPress={() => removeExercise(exercise.id)}
@@ -149,23 +170,121 @@ export default function WorkoutScreen() {
 }
 
 const styles = StyleSheet.create({
-  page: {flex: 1,backgroundColor: '#fff'},
-  scrollView: {flex: 1,backgroundColor: '#fff',},
-  container: {paddingTop: 80,paddingHorizontal: 24,paddingBottom: 40,alignItems: 'stretch',},
-  title: {fontSize: 32,fontWeight: '700',marginBottom: 20,},
-  routineInput: {borderWidth: 1,borderColor: '#ccc',paddingVertical: 14,paddingHorizontal: 16,borderRadius: 8,marginBottom: 24,fontSize: 16,backgroundColor: '#fafafa',},
-  card: {backgroundColor: '#fff',padding: 20,borderRadius: 16,borderWidth: 1,borderColor: '#ddd',marginBottom: 30,},
-  headerRow: {flexDirection: 'row',justifyContent: 'space-between',paddingBottom: 12,borderBottomWidth: 1,borderColor: '#eee',},
-  headerText: { fontWeight: '600',width: '30%',fontSize: 18,textAlign: 'center',},
-  inputRow: {flexDirection: 'row',alignItems: 'center',marginTop: 15,},
-  inputExercise: {flex: 2,borderWidth: 1,borderColor: '#ddd',borderRadius: 8,paddingVertical: 10,paddingHorizontal: 14,marginRight: 12,fontSize: 15,backgroundColor: '#f5f5f5',},
-  inputSmall: {flex: 1,borderWidth: 1,borderColor: '#ddd',borderRadius: 8,paddingVertical: 10,paddingHorizontal: 12,marginRight: 12,fontSize: 15,backgroundColor: '#f5f5f5',},
-  addButton: {marginTop: 20,alignSelf: 'flex-start',paddingVertical: 10,paddingHorizontal: 100,backgroundColor: '#e0f0ff',borderRadius: 30,},
-  addButtonText: {fontSize: 16,fontWeight: '400',color: '#007AFF',},
-  footer: {paddingHorizontal: 24,paddingBottom: 20,paddingTop: 10,backgroundColor: '#fff',},
-  timerButton: {backgroundColor: '#4a90e2',paddingVertical: 10,borderRadius: 20,alignItems: 'center',marginBottom: 12,},
-  backButton: {backgroundColor: '#BBBBBB',},
-  timerText: {color: '#fff',fontWeight: '600',fontSize: 17, },
-  deleteButton: {paddingHorizontal: 8,paddingVertical: 8,justifyContent: 'center',alignItems: 'center',},
-  deleteButtonText: {fontSize: 18,color: '#FF3B30',fontWeight: 'bold',},
+  page: { flex: 1, backgroundColor: '#fff' },
+  scrollView: { flex: 1, backgroundColor: '#fff' },
+  container: {
+    paddingTop: 80,
+    paddingHorizontal: 24,
+    paddingBottom: 40,
+    alignItems: 'stretch',
+  },
+  title: { fontSize: 32, fontWeight: '700', marginBottom: 20 },
+  routineInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 14,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    marginBottom: 24,
+    fontSize: 16,
+    backgroundColor: '#fafafa',
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    marginBottom: 30,
+  },
+  headerRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    paddingHorizontal: 5,
+  },
+  headerTextExercise: {
+    width: '50%',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  headerTextSmall: {
+    width: '22%',
+    fontWeight: '600',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  inputRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    paddingHorizontal: 5,
+  },
+  inputExercise: {
+    width: '50%',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    marginRight: 5,
+    fontSize: 15,
+    backgroundColor: '#f5f5f5',
+  },
+  inputSmall: {
+    width: '22%',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    marginRight: 3,
+    fontSize: 15,
+    backgroundColor: '#f5f5f5',
+  },
+  deleteButton: {
+    paddingHorizontal: 8,
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  deleteButtonText: {
+    fontSize: 19,
+    color: '#FF3B30',
+    fontWeight: 'bold',
+  },
+  addButton: {
+    marginTop: 20,
+    alignSelf: 'flex-start',
+    paddingVertical: 10,
+    paddingHorizontal: 100,
+    backgroundColor: '#e0f0ff',
+    borderRadius: 30,
+  },
+  addButtonText: {
+    fontSize: 13,
+    fontWeight: '400',
+    color: '#007AFF',
+  },
+  footer: {
+    paddingHorizontal: 24,
+    paddingBottom: 20,
+    paddingTop: 10,
+    backgroundColor: '#fff',
+  },
+  timerButton: {
+    backgroundColor: '#4a90e2',
+    paddingVertical: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  backButton: {
+    backgroundColor: '#BBBBBB',
+  },
+  timerText: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 17,
+  },
 });
